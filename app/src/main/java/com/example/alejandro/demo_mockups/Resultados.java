3d225498a338c4376d9f1d20e123996af1b76c94
Alejandro Garcia <alexgarciasanz1995@gmail.com>
2017-03-07 20:56:49 +0100

    Primer commit APP

---
+++ app/src/main/java/com/example/alejandro/demo_mockups/Resultados.java
@@ -0,0 +1,23 @@
+package com.example.alejandro.demo_mockups;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Resultados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
