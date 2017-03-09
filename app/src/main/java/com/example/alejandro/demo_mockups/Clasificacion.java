290ae54a1e80fc167d11d21da161f36f75a4d46a
Alejandro Garcia <alexgarciasanz1995@gmail.com>
2017-03-09 19:25:27 +0100

    ListView i Tabulacions en equipos, problema del click solucionat

--- app/src/main/java/com/example/alejandro/demo_mockups/Clasificacion.java
+++ app/src/main/java/com/example/alejandro/demo_mockups/Clasificacion.java
@@ -1,15 +1,23 @@
 package com.example.alejandro.demo_mockups;
 
+import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class Clasificacion extends AppCompatActivity {
-
+    ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +31,26 @@ public class Clasificacion extends AppCompatActivity {
                 finish();
             }
         });
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Pilotos"));
+        tabs.addTab(tabs.newTab().setText("Equipos"));
+
+
+        listview = (ListView) findViewById(R.id.list);
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listview.setAdapter(adapter);
+
+
     }
     private void setupActionBar(){
 
